ca5f4ce1d2c734711eb14048774f03c75a40de8f
Nick-Lemy <n.kayiranga@alustudent.com>
2026-03-03 15:35:54 +0200

    feat: add SerializeInterceptor for transforming response data with DTOs

---
+++ src/common/interceptors/serialize.interceptor.ts
@@ -0,0 +1,27 @@
+import {
+  Injectable,
+  NestInterceptor,
+  ExecutionContext,
+  CallHandler,
+} from '@nestjs/common';
+import { plainToInstance } from 'class-transformer';
+import { map, Observable } from 'rxjs';
+
+@Injectable()
+export class SerializeInterceptor<T> implements NestInterceptor {
+  constructor(private dto: new (...args: any[]) => T) {}
+  intercept(_context: ExecutionContext, next: CallHandler): Observable<any> {
+    return next.handle().pipe(
+      map((data) => {
+        if (Array.isArray(data)) {
+          return data.map((item) =>
+            plainToInstance(this.dto, item, { excludeExtraneousValues: true }),
+          );
+        }
+        return plainToInstance(this.dto, data, {
+          excludeExtraneousValues: true,
+        });
+      }),
+    );
+  }
+}
