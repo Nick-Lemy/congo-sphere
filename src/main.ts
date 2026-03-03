18a8999756c97448200941f56f27dc9cf8037b75
Nick-Lemy <n.kayiranga@alustudent.com>
2026-03-03 14:17:01 +0200

    feat: add @nestjs/swagger for API documentation and setup Swagger in main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import 'dotenv/config';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Congo Sphere')
+    .setDescription(
+      "Swagger for Congo's best event Registration and Management App",
+    )
+    .setVersion('0.1.1')
+    .addTag('Event')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
+
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
