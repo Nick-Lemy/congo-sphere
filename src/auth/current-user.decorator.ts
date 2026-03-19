869ce0ca1a0ce3a0cbdfab70f5bf06e4c2c585f6
Nick-Lemy <n.kayiranga@alustudent.com>
2026-03-19 16:37:25 +0200

    feat: update CurrentUser decorator to use a more descriptive parameter name

--- src/auth/current-user.decorator.ts
+++ src/auth/current-user.decorator.ts
@@ -2,7 +2,7 @@ import { createParamDecorator, ExecutionContext } from '@nestjs/common';
 import { JwtPayload } from '../common/types/jtw.type';
 
 export const CurrentUser = createParamDecorator(
-  (data: unknown, context: ExecutionContext) => {
+  (_data: unknown, context: ExecutionContext) => {
     const request = context
       .switchToHttp()
       .getRequest<Request & { user: JwtPayload }>();
