9fc263cc611eaf6a571ef238860a4a63546d1016
Nick-Lemy <n.kayiranga@alustudent.com>
2026-03-20 12:51:39 +0200

    feat: add cloudinary integration for image uploads in FileService

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -1,10 +1,18 @@
-import { Injectable } from '@nestjs/common';
-
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import { v2 as cloudinary } from 'cloudinary';
+import { unlink, writeFile } from 'fs/promises';
 @Injectable()
 export class FileService {
-  async uploadImage(file: Express.Multer.File): Promise<string> {
-    return new Promise((resolve) => {
-      resolve('File Name: ' + file.originalname);
-    });
+  async uploadImage(file: Express.Multer.File) {
+    const tmpPath = `./tmp/${file.originalname}`;
+    try {
+      await writeFile(tmpPath, file.buffer);
+      const imageUrl = await cloudinary.uploader.upload(tmpPath);
+      await unlink(tmpPath);
+      return imageUrl.url;
+    } catch (error) {
+      console.log(error);
+      throw new InternalServerErrorException();
+    }
   }
 }
