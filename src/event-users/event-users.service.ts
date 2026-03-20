9da5dae1fa1cb44559bb9131904fa490ca463fb8
Nick-Lemy <n.kayiranga@alustudent.com>
2026-03-21 01:57:10 +0200

    feat: implement event registration and cancellation methods with error handling

--- src/event-users/event-users.service.ts
+++ src/event-users/event-users.service.ts
@@ -1,19 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
 import { CreateEventUsersDto } from './dto/create-event-users.dto';
 
 @Injectable()
 export class EventUsersService {
   constructor(private readonly prisma: PrismaService) {}
-  create(createEventUsers: CreateEventUsersDto) {
-    const { eventId, userId, ...rest } = createEventUsers;
-    return this.prisma.eventUser.create({
-      data: {
-        ...rest,
-        joinedAt: new Date(),
-        event: { connect: { id: eventId } },
-        user: { connect: { id: userId } },
-      },
-    });
+  async create(createEventUsersDto: CreateEventUsersDto) {
+    const { eventId, userId, ...rest } = createEventUsersDto;
+    try {
+      const existingAttendee = await this.prisma.eventUser.findUnique({
+        where: { userId_eventId: { userId, eventId } },
+      });
+      if (existingAttendee)
+        throw new ConflictException(
+          'You are already a Participant of this event',
+        );
+      const attendee = await this.prisma.eventUser.create({
+        data: {
+          ...rest,
+          joinedAt: new Date(),
+          event: { connect: { id: eventId } },
+          user: { connect: { id: userId } },
+        },
+      });
+      return attendee;
+    } catch (error) {
+      console.warn('Error while creating an attendee: ', error);
+      throw new InternalServerErrorException(
+        'Error while creating an attendee',
+      );
+    }
   }
 }
--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,4 +1,5 @@
 import {
+  ConflictException,
   Injectable,
   InternalServerErrorException,
   NotFoundException,
@@ -80,5 +81,46 @@ export class EventsService {
     const event = await this.findOne(id);
     if (!event) throw new NotFoundException();
     return this.prisma.event.delete({ where: { id: event.id } });
+  }
+
+  async registerToEvent(eventId: string, user: JwtPayload) {
+    try {
+      const event = await this.findOne(eventId);
+      if (!event) {
+        throw new NotFoundException('Event not found!');
+      }
+
+      const attendee = await this.eventUsersService.create({
+        eventId,
+        userId: user.sub,
+        role: EventRole.ATTENDEE,
+      });
+
+      return attendee;
+    } catch (error) {
+      console.warn("Couldn't register user to event: ", error);
+      throw new InternalServerErrorException("User didn't register to event");
+    }
+  }
+
+  async cancelRegistration(eventId: string, user: JwtPayload) {
+    try {
+      const attendee = await this.prisma.eventUser.findFirst({
+        where: { eventId, userId: user.sub },
+      });
+
+      if (!attendee) throw new NotFoundException('Attendee not found!');
+      if (attendee.role === EventRole.HOST)
+        throw new ConflictException("Admin Can't Cancell registration");
+
+      return this.prisma.eventUser.delete({
+        where: { userId_eventId: { userId: user.sub, eventId } },
+      });
+    } catch (error) {
+      console.warn('Error while Cancelling registration: ', error);
+      throw new InternalServerErrorException(
+        'Impossible to cancel Registration',
+      );
+    }
   }
 }
