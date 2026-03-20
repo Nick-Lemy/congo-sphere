eaaa09057eb44ce309664382ef3b65a308de51ec
Nick-Lemy <n.kayiranga@alustudent.com>
2026-03-20 12:56:30 +0200

    refactor: improve image upload method and constructor configuration

--- src/file/file.service.ts
+++ src/file/file.service.ts
@@ -3,13 +3,21 @@ import { v2 as cloudinary } from 'cloudinary';
 import { unlink, writeFile } from 'fs/promises';
 @Injectable()
 export class FileService {
+  constructor() {
+    cloudinary.config({
+      cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+      api_key: process.env.CLOUDINARY_API_KEY,
+      api_secret: process.env.CLOUDINARY_API_SECRET,
+    });
+  }
+
   async uploadImage(file: Express.Multer.File) {
     const tmpPath = `./tmp/${file.originalname}`;
     try {
       await writeFile(tmpPath, file.buffer);
-      const imageUrl = await cloudinary.uploader.upload(tmpPath);
+      const { url: imageUrl } = await cloudinary.uploader.upload(tmpPath);
       await unlink(tmpPath);
-      return imageUrl.url;
+      return imageUrl;
     } catch (error) {
       console.log(error);
       throw new InternalServerErrorException();
