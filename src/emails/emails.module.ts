74027952d88796b7f894209517068ec6ca0383b2
Nick-Lemy <n.kayiranga@alustudent.com>
2026-03-21 17:10:34 +0200

    feat: add MailerModule integration for email functionality

--- src/emails/emails.module.ts
+++ src/emails/emails.module.ts
@@ -1,9 +1,26 @@
 import { Module } from '@nestjs/common';
 import { EmailsService } from './emails.service';
 import { EmailsController } from './emails.controller';
+import { MailerModule } from '@nestjs-modules/mailer';
+import 'dotenv/config';
 
 @Module({
   controllers: [EmailsController],
   providers: [EmailsService],
+  imports: [
+    MailerModule.forRootAsync({
+      useFactory: () => ({
+        transport: {
+          host: process.env.SMTP_HOST,
+          port: parseInt(process.env.SMTP_PORT) ?? ,
+          secure: false,
+          auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS,
+          },
+        },
+      }),
+    }),
+  ],
 })
 export class EmailsModule {}
