8c70fd5adb39a4a26d92a9dbd64a3b2aa0aa4164
Nick-Lemy <n.kayiranga@alustudent.com>
2026-03-23 16:51:39 +0200

    feat: implement email sending functionality in EmailsService

--- src/emails/emails.service.ts
+++ src/emails/emails.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { createTransport } from 'nodemailer';
 
 @Injectable()
-export class EmailsService {}
+export class EmailsService {
+  constructor() {}
+  private createEmailTransporter() {
+    return createTransport({
+      service: 'Gmail',
+      host: process.env.EMAIL_HOST,
+      port: Number(process.env.EMAIL_PORT),
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  }
+
+  async sendEmail(
+    to: string,
+    subject: string,
+    html: string,
+    attachments?: { filename: string; path: string }[],
+  ) {
+    const transporter = this.createEmailTransporter();
+    await transporter.sendMail({
+      from: process.env.EMAIL_USER,
+      to,
+      subject,
+      html,
+      attachments,
+    });
+  }
+}
